54bcdc1bf20032232bc0512f3d54a9082dd3cb70
Ivan Aphanasyev <nekaktotak2@gmail.com>
2019-08-15 22:23:39 +0300

    fix loggerConfig

--- config/loggerConfig.js
+++ config/loggerConfig.js
@@ -1,10 +1,11 @@
 const logger = require("morgan");
 const rfs = require("rotating-file-stream");
 const path = require("path");
+const appRoot = require("app-root-path");
 
 const accesLoggerStream = rfs("access.log", {
   interval: "1d",
-  path: path.join(__dirname, "logs")
+  path: path.join(appRoot.path, "logs")
 });
 const consoleLogger = logger("dev");
 const fileLogger = logger("combined", { stream: accesLoggerStream });
